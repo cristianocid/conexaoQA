d1b6a4c1871e80142809d56f3c6e3f78989cd12a
Cristiano Cid Bonfim <57623544+cristianocid@users.noreply.github.com>
2023-03-15 18:50:58 -0300

    Create dev.js

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: "mongodb+srv://cristianobonfim:aAyLLnQpfMVpUPIJ@cluster0.sfus8ev.mongodb.net/?retryWrites=true&w=majority",
+    jwtSecret: "cristianoCid",
+    githubClientId: "b5d691b85f26d6822294",
+    githubSecret: "58e2963d8d2bd04f8429f25b4ed154356ee39fe5"
+}
